37500f574972b9cb6dbc093e227ce1c55708972b
Callym <hi@callym.com>
2017-12-14 19:43:51 +0000

    Coordinate => RectangularCoordinate

--- src/coordinates.rs
+++ src/coordinates.rs
@@ -16,12 +16,26 @@ impl From<RectangularCoordinates> for Coordinates {
 		let r = (x.powi(2) + y.powi(2) + z.powi(2)).sqrt();
 		let ra = y.atan2(x).to_degrees();
 		let ra = if ra >= 0.0 { ra } else { ra + 360.0 };
-		let lat = (z / r).asin().to_degrees();
+		// http://www.stjarnhimlen.se/comp/tutorial.html
+		let lat = z.atan2((x.powi(2) + y.powi(2)).sqrt()).to_degrees();
 
 		Self {
 			lat: lat,
 			lon: ra,
 			dist: r,
+		}
+	}
+}
+
+impl From<Coordinates> for RectangularCoordinates {
+	fn from(coord: Coordinates) -> Self {
+		let lon = coord.lon.to_radians();
+		let lat = coord.lat.to_radians();
+
+		Self {
+			x: coord.dist * lon.cos() * lat.cos(),
+			y: coord.dist * lon.sin() * lat.cos(),
+			z: coord.dist * lat.sin()
 		}
 	}
 }
